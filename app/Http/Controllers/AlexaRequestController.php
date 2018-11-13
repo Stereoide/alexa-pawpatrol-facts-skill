66658ead4010772dcb0381975ea27104bd87e757
Jörn Maske <j.maske@otterbach.de>
2018-11-13 21:45:04 +0100

    Prepare to verify signature

--- app/Http/Controllers/AlexaRequestController.php
+++ app/Http/Controllers/AlexaRequestController.php
@@ -45,7 +45,7 @@ class AlexaRequestController extends Controller
         openssl_public_decrypt($signature, $certificateHash, $certificatePublicKey);
 
         $response = new Response();
-        return $response->withOutputSpeech(new OutputSpeech('Certificate signature: ' . $signature))->render();
+        return $response->withOutputSpeech(new OutputSpeech('Certificate signature is ' . strlen($signature) . ' characters long'))->render();
 
         $response = new Response();
         return $response->withOutputSpeech(new OutputSpeech('Certificate keys: ' . implode(', ', array_keys($certificate))))->render();
