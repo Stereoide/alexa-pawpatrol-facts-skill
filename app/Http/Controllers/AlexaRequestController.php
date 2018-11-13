efb5fe2e0d4b2eb7496daf8bc58658e1f2a5de62
Jörn Maske <j.maske@otterbach.de>
2018-11-13 22:32:35 +0100

    Prepare to verify signature

--- app/Http/Controllers/AlexaRequestController.php
+++ app/Http/Controllers/AlexaRequestController.php
@@ -48,7 +48,7 @@ class AlexaRequestController extends Controller
         $sha1Hash = sha1($requestBody);
 
         $response = new Response();
-        return $response->withOutputSpeech(new OutputSpeech(substr($signature, 0, 10)))->render();
+        return $response->withOutputSpeech(new OutputSpeech(strlen($certificateHash)))->render();
 
         $response = new Response();
         return $response->withOutputSpeech(new OutputSpeech('Certificate keys: ' . implode(', ', array_keys($certificate))))->render();
